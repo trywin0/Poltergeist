7f87395b74b0ee7f59c64bf7e6ffcf1fbef1ca27
Trywin <trywinepic@gmail.com>
2021-02-06 17:14:10 +0100

    Added some background animations and a back button

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ function showAbout() {
   const aboutDiv = document.getElementById("about")
   aboutDiv.style.display = "block"
 }
+function hideAbout() {
+  const entranceDiv = document.getElementById("entrance")
+  entranceDiv.style.display = "block"
+
+  const aboutDiv = document.getElementById("about")
+  aboutDiv.style.display = "none"
+}
 
 setTimeout(() => {
   document.getElementById("enterbtn").style.cursor = "pointer"
